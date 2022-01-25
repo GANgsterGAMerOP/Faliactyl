4f67561fd8d8731cb42ef19cc52cbaabfc6149e1
RianOP <87343593+RianOP@users.noreply.github.com>
2022-01-25 15:39:01 -0500

    Added a few things

--- api/slownet.js
+++ api/slownet.js
@@ -1,5 +1,5 @@
 module.exports.load = async function(app, db) {
  app.get("/slownet", async (req, res) => {
-  res.send("Your network is too slow to load Faliactyl! Trying again in 5 seconds...<script>window.location.href='/dashboard'</script>") 
+  res.send("Your network is too slow to load heliactyl! Trying again in 5 seconds...<script>window.location.href='/dashboard'</script>") 
  }) 
 }
